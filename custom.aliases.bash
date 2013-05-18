37341997761068c1f15ccefd6d32f1d93045a526
jasdeepkhalsa <jasdeep@simplyspiritdesign.com>
2013-05-18 17:26:29 +0100

    added custom aliases

---
+++ custom.aliases.bash
@@ -0,0 +1,9 @@
+cite about-alias
+about-alias 'my custom aliases'
+
+alias default='sudo a2dissite mrlawyer && sudo a2ensite default && sudo service apache2 reload'
+alias mrlawyer='sudo a2dissite default && sudo a2ensite mrlawyer && sudo service apache2 reload'
+alias code='cd /files/code'
+alias nd='NODE_ENV=dev node app.js'
+alias selenium='java -jar /files/code/lib/selenium-server-standalone-2.32.0.jar'
+alias refresh='bash-it disable alias custom && bash-it enable alias custom'
